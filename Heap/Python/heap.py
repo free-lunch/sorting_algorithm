85f407aa3b46e015f3c84f630f54910232bb3e0d
free-lunch <jjh783@gmail.com>
2016-09-19 18:43:43 +0900

    Add a heap sorting on python

---
+++ Heap/Python/heap.py
@@ -0,0 +1,40 @@
+def compare_item(i, j, reverse=False):
+    if not reverse:
+        return i < j
+    else:
+        return i > j
+
+
+def heap_sort(items, reverse=False):
+    print items
+    heapify(items, reverse)
+
+    index = len(items)-1
+    while index > 0:
+        items[index], items[0] = items[0], items[index]
+        index -= 1
+        shift_down(items, 0, index, reverse)
+
+
+def heapify(items, reverse):
+    for start in range((len(items)-2)//2, -1, -1):
+        shift_down(items, start, len(items)-1, reverse)
+        print items
+
+
+def shift_down(items, start, end, reverse):
+    root = start
+    while True:
+        child = root*2+1
+        if child > end:
+            break
+
+        if child+1 <= end and compare_item(items[child], items[child+1], reverse):
+            child += 1
+
+        if compare_item(items[root], items[child], reverse):
+            items[root], items[child] = items[child], items[root]
+            root = child
+        else:
+            break
+
---
+++ Heap/Python/heap_test.py
@@ -0,0 +1,27 @@
+import unittest
+from heap import heap_sort
+
+class HeapUnittest(unittest.TestCase):
+    @staticmethod
+    def checkValid(items, reverse=False):
+        n = len(items)
+        for i in xrange(0,n-1):
+            if not reverse and items[i] > items[i+1]:
+                return False
+            elif reverse and items[i] < items[i + 1]:
+                return False
+        return True
+
+    def test_Sort(self):
+        items = [1,5,4,2,8,9,10]
+        heap_sort(items)
+        self.assertEquals(True, self.checkValid(items))
+
+    def test_ReverseSort(self):
+        items = [1, 5, 4, 2, 8, 9, 10]
+        heap_sort(items, reverse=True)
+        self.assertEquals(True, self.checkValid(items, reverse=True))
+
+
+if __name__ == "__main__":
+    unittest.main()
